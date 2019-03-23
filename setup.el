4a2d2cf2a66be2075b62e995e85b6f54df46d41a
pedh <hcn518@gmail.com>
2019-03-23 10:02:09 +0800

    Initial commit

---
+++ setup.el
@@ -0,0 +1,33 @@
+(require 'ox-publish)
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((dot . t)))
+
+(setq org-confirm-babel-evaluate nil)
+
+(setq org-publish-project-alist
+      '(("html"
+	 :base-directory "."
+	 :base-extension "org"
+	 :publishing-directory "_build/html"
+	 :exclude "README\\.org\\|_build"
+	 :recursive t
+	 :publishing-function org-html-publish-to-html
+	 :author "pedh"
+	 :email "hcn518@gmail.com"
+	 :section-numbers nil
+	 :auto-sitemap t
+	 :sitemap-filename "index.org"
+	 :sitemap-title "SICP Solutions"
+	 :html-link-up "/SICP-Solutions"
+	 :html-link-home "/SICP-Solutions"
+	 :html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"/css/org-info.css\" />"
+	 :infojs-opt "path:/js/org-info.js")
+	("static"
+	 :base-directory "."
+	 :base-extension "py\\|c\\|png\\|pdf\\|chm"
+	 :publishing-directory "_build/html"
+	 :recursive t
+	 :publishing-function org-publish-attachment
+	 )))
